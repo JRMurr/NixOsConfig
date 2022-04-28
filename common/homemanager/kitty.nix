4562ee97f73a74289b0b14b80259ae4d495ad5ce
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-04-28 18:48:23 -0400

    more graphics gating

--- common/homemanager/kitty.nix
+++ common/homemanager/kitty.nix
@@ -1,37 +1,41 @@
-{ pkgs, config, lib, ... }: {
-  programs.kitty = {
-    enable = true;
-    settings = {
-      font_family = "FiraCode Nerd Font";
-      bold_font = "auto";
-      italic_font = "auto";
-      bold_italic_font = "auto";
-      enable_audio_bell = false;
-      scrollback_lines = -1;
+{ pkgs, config, lib, nixosConfig, ... }:
+let gcfg = nixosConfig.myOptions.graphics;
+in {
+  config = lib.mkIf gcfg.enable {
+    programs.kitty = {
+      enable = true;
+      settings = {
+        font_family = "FiraCode Nerd Font";
+        bold_font = "auto";
+        italic_font = "auto";
+        bold_italic_font = "auto";
+        enable_audio_bell = false;
+        scrollback_lines = -1;
+      };
+      # theme dracula
+      extraConfig = ''
+        background #1e1f28
+        foreground #f8f8f2
+        cursor #bbbbbb
+        selection_background #44475a
+        color0 #000000
+        color8 #545454
+        color1 #ff5555
+        color9 #ff5454
+        color2 #50fa7b
+        color10 #50fa7b
+        color3 #f0fa8b
+        color11 #f0fa8b
+        color4 #bd92f8
+        color12 #bd92f8
+        color5 #ff78c5
+        color13 #ff78c5
+        color6 #8ae9fc
+        color14 #8ae9fc
+        color7 #bbbbbb
+        color15 #ffffff
+        selection_foreground #1e1f28
+      '';
     };
-    # theme dracula
-    extraConfig = ''
-      background #1e1f28
-      foreground #f8f8f2
-      cursor #bbbbbb
-      selection_background #44475a
-      color0 #000000
-      color8 #545454
-      color1 #ff5555
-      color9 #ff5454
-      color2 #50fa7b
-      color10 #50fa7b
-      color3 #f0fa8b
-      color11 #f0fa8b
-      color4 #bd92f8
-      color12 #bd92f8
-      color5 #ff78c5
-      color13 #ff78c5
-      color6 #8ae9fc
-      color14 #8ae9fc
-      color7 #bbbbbb
-      color15 #ffffff
-      selection_foreground #1e1f28
-    '';
   };
 }
--- common/homemanager/rofi.nix
+++ common/homemanager/rofi.nix
@@ -1,17 +1,20 @@
-{ pkgs, lib, config, ... }: {
-
-  # try to use stuff from https://github.com/adi1090x/rofi
-  programs.rofi = {
-    enable = true;
-    theme = "Arc-Dark";
-    terminal = "${pkgs.kitty}/bin/kitty";
-    extraConfig = {
-      # https://github.com/davatorium/rofi/blob/next/doc/rofi.1.markdown
-      modi = "run,window,ssh";
-      cache-dir = "~/.cache/rofi";
-      matching = "fuzzy";
-      run-shell-command =
-        "{terminal} --hold {cmd}"; # this is kitty only option on terminal, need to hit shift+return for shell commands to run
+{ pkgs, lib, config, nixosConfig, ... }:
+let gcfg = nixosConfig.myOptions.graphics;
+in {
+  config = lib.mkIf gcfg.enable {
+    # try to use stuff from https://github.com/adi1090x/rofi
+    programs.rofi = {
+      enable = true;
+      theme = "Arc-Dark";
+      terminal = "${pkgs.kitty}/bin/kitty";
+      extraConfig = {
+        # https://github.com/davatorium/rofi/blob/next/doc/rofi.1.markdown
+        modi = "run,window,ssh";
+        cache-dir = "~/.cache/rofi";
+        matching = "fuzzy";
+        run-shell-command =
+          "{terminal} --hold {cmd}"; # this is kitty only option on terminal, need to hit shift+return for shell commands to run
+      };
     };
   };
 }
