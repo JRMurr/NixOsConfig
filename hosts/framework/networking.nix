061a450cc4fea27020deca80fd7bec411aea8b44
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-05-11 20:35:12 -0400

    change hostname

--- hosts/framework/networking.nix
+++ hosts/framework/networking.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }: {
   networking = {
-    hostName = "nixos-john";
+    hostName = "framework";
     networkmanager.enable = true;
     # The global useDHCP flag is deprecated, therefore explicitly set to false here.
     # Per-interface useDHCP will be mandatory in the future, so this generated config
