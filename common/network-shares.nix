6d6b96285be6b68c9e278c91457a8848974f4149
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-07-28 20:28:03 -0400

    update flake

--- common/network-shares.nix
+++ common/network-shares.nix
@@ -1,4 +1,9 @@
 { pkgs, config, ... }: {
+  services.gvfs.enable = true;
+  environment.systemPackages = with pkgs;
+    [
+      lxqt.lxqt-policykit
+    ]; # provides a default authentification client for policykitcle
   fileSystems."/mnt/games" = {
     device = "//192.168.1.151/games";
     fsType = "cifs";
@@ -7,7 +12,7 @@
       automount_opts =
         "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
       uid = "1000";
-      gid = "1"; # wheel
+      gid = "100"; # users
     in [
       "${automount_opts},credentials=/etc/nixos/secrets/smb,uid=${uid},gid=${gid}"
     ];
