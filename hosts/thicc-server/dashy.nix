141bee1813cb9b9a2b031cb9a6c1e1533d12f85f
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-08-14 17:45:16 -0400

    add dashy

---
+++ hosts/thicc-server/dashy.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }:
+let
+  containerCfg = config.myOptions.containers;
+  configDir = containerCfg.dataDir;
+in {
+  config = lib.mkIf containerCfg.enable {
+
+    virtualisation.oci-containers.containers."Dashy" = {
+      autoStart = true;
+      image = "lissy93/dashy:latest";
+      environment = {
+        "NODE_ENV" = "production";
+        # TODO: use nix options to get right ids
+        "UID" = "1000"; # jr
+        "GID" = "131"; # docker
+      };
+      ports = [ "80:80" ];
+      volumes = [ "${configDir}/dashy/my-config.yml:/app/public/conf.yml" ];
+      extraOptions = [ "--restart=unless-stopped" ];
+    };
+
+    virtualisation.oci-containers.containers."monitoring" = {
+      autoStart = true;
+      image = "nicolargo/glances:latest-alpine";
+      environment = { "GLANCES_OPT" = "-w"; };
+      ports = [ " 61208:61208" "61209:61209" ];
+      volumes = [ "/var/run/docker.sock:/var/run/docker.sock" ];
+      extraOptions = [ "--restart=unless-stopped" ];
+    };
+  };
+}
--- hosts/thicc-server/pi-hole.nix
+++ hosts/thicc-server/pi-hole.nix
@@ -13,13 +13,19 @@ in {
         # TODO: make secrets, not a big deal for now
         "WEBPASSWORD" = "I8q22OUb";
       };
-      ports = [ "53:53/tcp" "53:53/udp" "80:80" ];
+      ports = [ "53:53/tcp" "53:53/udp" "81:80" ];
       volumes = [
         "${configDir}/pihole/etc:/etc/pihole"
         "${configDir}/pihole/dnsmasq:/etc/dnsmasq.d"
       ];
-      extraOptions =
-        [ "--dns" "1.1.1.1" "--dns" "127.0.0.1" "--cap-add=NET_ADMIN" ];
+      extraOptions = [
+        "--restart=unless-stopped"
+        "--dns"
+        "1.1.1.1"
+        "--dns"
+        "127.0.0.1"
+        "--cap-add=NET_ADMIN"
+      ];
     };
   };
 }
