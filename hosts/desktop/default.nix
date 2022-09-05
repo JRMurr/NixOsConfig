f49141a1d1e992cb32aa3ad1f0b23041b0ec828e
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-05 15:52:21 -0400

    enable ssh on desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -11,16 +11,13 @@
     ./gaming.nix
   ];
 
-  # Use the GRUB 2 boot loader.
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.version = 2;
-  boot.loader.systemd-boot.enable = true;
-  # boot.loader.grub.useOSProber = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "/dev/disk/by-uuid/BC77-ADCA"; # or "nodev" for efi only
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      consoleMode = "auto";
+    };
+    efi.canTouchEfiVariables = true;
+  };
 
   # Set your time zone.
   time.timeZone = "America/New_York";
@@ -37,7 +34,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
