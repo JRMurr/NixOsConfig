0847f0850c19d937d9419d69244d526fd9615938
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-09-05 16:55:08 -0400

    add serverData share

--- common/network-shares.nix
+++ common/network-shares.nix
@@ -1,20 +1,30 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, ... }:
+
+let
+  uid = "1000";
+  gid = "100"; # users
+in {
   services.gvfs.enable = true;
   environment.systemPackages = with pkgs;
     [
       lxqt.lxqt-policykit
     ]; # provides a default authentification client for policykitcle
-  fileSystems."/mnt/games" = {
-    device = "//192.168.1.151/games";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts =
-        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-      uid = "1000";
-      gid = "100"; # users
-    in [
-      "${automount_opts},credentials=/etc/nixos/secrets/smb,uid=${uid},gid=${gid}"
-    ];
+  # fileSystems."/mnt/games" = {
+  #   device = "//192.168.1.151/games";
+  #   fsType = "cifs";
+  #   options = let
+  #     # this line prevents hanging on network split
+  #     automount_opts =
+  #       "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+  #   in [
+  #     "${automount_opts},credentials=/etc/nixos/secrets/smb,uid=${uid},gid=${gid}"
+  #   ];
+  # };
+
+  fileSystems."/mnt/fatnas/serverdata" = {
+    device = "192.168.1.151:/volume1/ServerData";
+    fsType = "nfs";
+    # "uid=${uid}" "gid=${gid}" 
+    options = [ "x-systemd.automount" "noauto" ];
   };
 }
