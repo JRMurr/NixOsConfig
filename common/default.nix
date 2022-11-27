3e958784bb03e53cf5c3c9a2837dea40da701be1
John Murray <5672686+JRMurr@users.noreply.github.com>
2022-11-27 13:49:49 -0500

    add nix cache

--- common/default.nix
+++ common/default.nix
@@ -24,6 +24,12 @@
     settings = {
       trusted-users = [ "root" "jr" ];
       auto-optimise-store = true;
+      substituters =
+        [ "https://nix-community.cachix.org" "https://jrmurr.cachix.org" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "jrmurr.cachix.org-1:nE2/Ms3YbTPe8SrFOWsHfcNAuJtJtz9UCoohiSn6Elg="
+      ];
     };
     gc = {
       automatic = true;
