fc0d1052297cde50930920c7fe4e7cf52de2bde9
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-06-26 10:50:06 -0400

    fix nixd on mac

--- common/homemanager/programs.nix
+++ common/homemanager/programs.nix
@@ -1,5 +1,12 @@
 { pkgs, inputs, ... }:
 let
   nurl = inputs.nurl.packages.${pkgs.system}.default;
-  # nurl
-in { home.packages = with pkgs; [ bottom htop cachix nixd nurl dive ]; }
+  nixd = inputs.nixd.packages.${pkgs.system}.default;
+  # nixd does not work on mac yet :(
+  # https://github.com/nix-community/nixd/issues/107
+  linuxOnly = pkgs.lib.optionals pkgs.stdenv.isLinux [ nixd ];
+in {
+
+  home.packages = linuxOnly ++ (with pkgs; [ bottom htop cachix nurl dive ]);
+
+}
