7cac32426a24a3a14f6e8580d1b4e6efe26b8812
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-07-11 09:27:04 -0400

    tailscale option

--- common/myOptions/tailscale.nix
+++ common/myOptions/tailscale.nix
@@ -1,6 +1,7 @@
 { config, lib, ... }: {
   options = with lib; {
     myOptions.tailscale = {
+      enable = mkEnableOption "Enable tailscale";
       tailNetName = mkOption {
         type = types.str;
         default = "tail19e8e.ts.net";
--- common/tailscale.nix
+++ common/tailscale.nix
@@ -1,13 +1,15 @@
 { config, pkgs, ... }:
+let tailscaleCfg = config.myOptions.tailscale;
+in {
+  config = lib.mkIf tailscaleCfg.enable {
+    environment.systemPackages = [ pkgs.tailscale ];
 
-{
-  environment.systemPackages = [ pkgs.tailscale ];
+    services.tailscale.enable = true;
 
-  services.tailscale.enable = true;
+    networking.firewall.checkReversePath = "loose";
 
-  networking.firewall.checkReversePath = "loose";
+    networking.nameservers = [ "100.100.100.100" "1.1.1.1" "8.8.8.8" ];
+    networking.search = [ "johnreillymurray.gmail.com.beta.tailscale.net" ];
 
-  networking.nameservers = [ "100.100.100.100" "1.1.1.1" "8.8.8.8" ];
-  networking.search = [ "johnreillymurray.gmail.com.beta.tailscale.net" ];
-
+  };
 }
