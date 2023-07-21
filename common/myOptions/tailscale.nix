da669dfd3d9784be9029141ec59a802eee663906
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-07-20 21:41:33 -0400

    make tailscale deafult on

--- common/myOptions/tailscale.nix
+++ common/myOptions/tailscale.nix
@@ -1,7 +1,12 @@
 { config, lib, ... }: {
   options = with lib; {
     myOptions.tailscale = {
-      enable = mkEnableOption "Enable tailscale";
+      enable = mkOption {
+        default = true;
+        example = true;
+        description = "enable tailscale";
+        type = lib.types.bool;
+      };
       tailNetName = mkOption {
         type = types.str;
         default = "tail19e8e.ts.net";
