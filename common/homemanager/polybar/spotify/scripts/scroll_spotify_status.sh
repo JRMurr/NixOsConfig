94995760ad823c5fd0c4eac0d6208f24ba8b4764
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-07-26 14:43:21 -0400

    mess with format

--- common/homemanager/polybar/spotify/scripts/scroll_spotify_status.sh
+++ common/homemanager/polybar/spotify/scripts/scroll_spotify_status.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+#         # --before-text "%{u#1db954}%{+u}" \
 # see man zscroll for documentation of the following parameters
 zscroll -l 30 \
-        --delay 0.1 \
+        --delay 0.3 \
         --scroll-padding " " \
         --match-command "get_spotify_status --status" \
         --match-text "Playing" "--scroll 1" \
         --match-text "Paused" "--scroll 0" \
         --update-check true "get_spotify_status" &
-
 wait
