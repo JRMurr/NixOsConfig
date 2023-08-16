4e9a1e5f9cba7d0e786de60bce5c44f383cb5ffe
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-08-16 14:08:23 -0400

    mess with starship

--- common/homemanager/starship.nix
+++ common/homemanager/starship.nix
@@ -1,8 +1,9 @@
-{ pkgs, ... }: {
+{ lib, ... }: {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
     enableBashIntegration = true;
+    # enableTransience = true;
     settings = {
       # stolen from https://draculatheme.com/starship dont think its needed
       # aws.style = "bold #ffb86c";
@@ -25,6 +26,103 @@
         min_time = 500;
         show_milliseconds = true;
       };
+
+      time = { disabled = false; };
+
+      jobs.disabled = true;
+
+      format = let
+        # https://starship.rs/config/#prompt
+        modules = [
+          # displayed in order
+          "username"
+          "hostname"
+          "localip"
+          "shlvl"
+          "singularity"
+          "kubernetes"
+          "directory"
+          "vcsh"
+          "fossil_branch"
+          "git_branch"
+          "git_commit"
+          "git_state"
+          "git_metrics"
+          "git_status"
+          "hg_branch"
+          "pijul_channel"
+          "docker_context"
+          "package"
+          "c"
+          "cmake"
+          "cobol"
+          "daml"
+          "dart"
+          "deno"
+          "dotnet"
+          "elixir"
+          "elm"
+          "erlang"
+          "fennel"
+          "golang"
+          "guix_shell"
+          "haskell"
+          "haxe"
+          "helm"
+          "java"
+          "julia"
+          "kotlin"
+          "gradle"
+          "lua"
+          "nim"
+          "nodejs"
+          "ocaml"
+          "opa"
+          "perl"
+          "php"
+          "pulumi"
+          "purescript"
+          "python"
+          "raku"
+          "rlang"
+          "red"
+          "ruby"
+          "rust"
+          "scala"
+          "solidity"
+          "swift"
+          "terraform"
+          "vlang"
+          "vagrant"
+          "zig"
+          "buf"
+          "nix_shell"
+          "conda"
+          "meson"
+          "spack"
+          "memory_usage"
+          "aws"
+          "gcloud"
+          "openstack"
+          "azure"
+          "env_var"
+          "crystal"
+          "custom"
+          "sudo"
+          "time" # moved this
+          "cmd_duration"
+          "line_break"
+          "jobs"
+          "battery"
+          # "time"
+          "status"
+          "os"
+          "container"
+          "shell"
+          "character"
+        ];
+      in lib.concatMapStrings (m: "$" + m) modules;
+
     };
   };
 }
