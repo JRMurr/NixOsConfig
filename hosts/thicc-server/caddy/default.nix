c1d3e88271a9024bbf782d8c91c9ef24188e4dbb
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-12-13 23:20:40 -0500

    update

--- hosts/thicc-server/caddy/default.nix
+++ hosts/thicc-server/caddy/default.nix
@@ -97,7 +97,7 @@ in {
     email = "johnreillymurray@gmail.com";
     package = pkgs.caddyWithPlugins.override (prev: {
       plugins = [{ name = "github.com/caddy-dns/cloudflare"; }];
-      vendorHash = "sha256-uyEjAktinJhV3u5xFWAHbBPAX5NZ5utLiCwUVgZVjGw=";
+      vendorHash = "sha256-LxnYnkFpQFPZfEcJOMJSTkJ3pLSp7VXZUDMf6iH9qk4=";
     });
     logFormat = lib.mkForce "level info";
     virtualHosts = let
