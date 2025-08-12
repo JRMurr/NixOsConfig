b31a253d68932c3035f5d3a8e80f8a9b411e9333
John Murray <5672686+JRMurr@users.noreply.github.com>
2025-08-12 10:36:30 -0400

    update

--- common/ssh.nix
+++ common/ssh.nix
@@ -2,5 +2,5 @@
   # https://www.reddit.com/r/NixOS/comments/lsbo9a/people_using_sshagent_how_do_you_unlock_it_on/?context=3
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.lightdm.enableGnomeKeyring = true;
-  programs.ssh.startAgent = true;
+  # programs.ssh.startAgent = true;
 }
