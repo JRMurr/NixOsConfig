a44adf65d62d9cdae85a16f5b0e0aa174cd95eef
John Murray <5672686+JRMurr@users.noreply.github.com>
2025-08-18 19:41:08 -0400

    move more config to nix

--- common/homemanager/hyprland/default.nix
+++ common/homemanager/hyprland/default.nix
@@ -95,6 +95,92 @@ in
         "$fileManager" = "dolphin";
         "$menu" = "rofi -show run";
 
+        #####################
+        ### LOOK AND FEEL ###
+        #####################
+
+        # Refer to https://wiki.hypr.land/Configuring/Variables/
+
+        # https://wiki.hypr.land/Configuring/Variables/#general
+        general = {
+          gaps_in = 5;
+          gaps_out = 10;
+
+          border_size = 1;
+
+          # https://wiki.hypr.land/Configuring/Variables/#variable-types for info about colors
+          "col.active_border" = "$accent"; # rgba(33ccffee) rgba(00ff99ee) 45deg
+          "col.inactive_border" = "rgba(595959aa)";
+
+          # Set to true enable resizing windows by clicking and dragging on borders and gaps
+          resize_on_border = false;
+
+          # Please see https://wiki.hypr.land/Configuring/Tearing/ before you turn this on
+          allow_tearing = false;
+
+          layout = "dwindle";
+        };
+
+        # https://wiki.hypr.land/Configuring/Variables/#decoration
+        decoration = {
+          rounding = 10;
+          rounding_power = 2;
+
+          # Focused / unfocused opacity
+          active_opacity = 1.0;
+          inactive_opacity = 1.0;
+
+          shadow = {
+            enabled = true;
+            range = 4;
+            render_power = 3;
+            color = "rgba(1a1a1aee)";
+          };
+
+          blur = {
+            enabled = true;
+            size = 3;
+            passes = 1;
+            vibrancy = 0.1696;
+          };
+        };
+
+        animations = {
+          enabled = true;
+
+          # Default curves, see https://wiki.hypr.land/Configuring/Animations/#curves
+          # NAME,           X0,   Y0,   X1,   Y1
+          bezier = [
+            "easeOutQuint,   0.23, 1,    0.32, 1"
+            "easeInOutCubic, 0.65, 0.05, 0.36, 1"
+            "linear,         0,    0,    1,    1"
+            "almostLinear,   0.5,  0.5,  0.75, 1"
+            "quick,          0.15, 0,    0.1,  1"
+          ];
+
+          # Default animations, see https://wiki.hypr.land/Configuring/Animations/
+          #  NAME,      ONOFF, SPEED, CURVE,        [STYLE]
+          animation = [
+            "global,        1, 10,   default"
+            "border,        1, 5.39, easeOutQuint"
+            "windows,       1, 4.79, easeOutQuint"
+            "windowsIn,     1, 4.1,  easeOutQuint, popin 87%"
+            "windowsOut,    1, 1.49, linear,       popin 87%"
+            "fadeIn,        1, 1.73, almostLinear"
+            "fadeOut,       1, 1.46, almostLinear"
+            "fade,          1, 3.03, quick"
+            "layers,        1, 3.81, easeOutQuint"
+            "layersIn,      1, 4,    easeOutQuint, fade"
+            "layersOut,     1, 1.5,  linear,       fade"
+            "fadeLayersIn,  1, 1.79, almostLinear"
+            "fadeLayersOut, 1, 1.39, almostLinear"
+            "workspaces,    1, 1.94, almostLinear, fade"
+            "workspacesIn,  1, 1.21, almostLinear, fade"
+            "workspacesOut, 1, 1.94, almostLinear, fade"
+            "zoomFactor,    1, 7,    quick"
+          ];
+        };
+
         monitorv2 = monitorLines;
         bind = [
           "$mainMod, P, pseudo" # dwindle pseudo-tile
@@ -182,6 +268,7 @@ in
           # idk if this works (it does not...)
           # "[workspace kitty-ws silent] kitty"
         ];
+
       };
 
       extraConfig = ''
@@ -229,92 +316,6 @@ in
         # permission = /usr/(bin|local/bin)/grim, screencopy, allow
         # permission = /usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland, screencopy, allow
         # permission = /usr/(bin|local/bin)/hyprpm, plugin, allow
-
-
-        #####################
-        ### LOOK AND FEEL ###
-        #####################
-
-        # Refer to https://wiki.hypr.land/Configuring/Variables/
-
-        # https://wiki.hypr.land/Configuring/Variables/#general
-        general {
-            gaps_in = 5
-            gaps_out = 20
-
-            border_size = 2
-
-            # https://wiki.hypr.land/Configuring/Variables/#variable-types for info about colors
-            col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
-            col.inactive_border = rgba(595959aa)
-
-            # Set to true enable resizing windows by clicking and dragging on borders and gaps
-            resize_on_border = false
-
-            # Please see https://wiki.hypr.land/Configuring/Tearing/ before you turn this on
-            allow_tearing = false
-
-            layout = dwindle
-        }
-
-        # https://wiki.hypr.land/Configuring/Variables/#decoration
-        decoration {
-            rounding = 10
-            rounding_power = 2
-
-            # Change transparency of focused and unfocused windows
-            active_opacity = 1.0
-            inactive_opacity = 1.0
-
-            shadow {
-                enabled = true
-                range = 4
-                render_power = 3
-                color = rgba(1a1a1aee)
-            }
-
-            # https://wiki.hypr.land/Configuring/Variables/#blur
-            blur {
-                enabled = true
-                size = 3
-                passes = 1
-
-                vibrancy = 0.1696
-            }
-        }
-
-        # https://wiki.hypr.land/Configuring/Variables/#animations
-        animations {
-            enabled = yes, please :)
-
-            # Default curves, see https://wiki.hypr.land/Configuring/Animations/#curves
-            #        NAME,           X0,   Y0,   X1,   Y1
-            bezier = easeOutQuint,   0.23, 1,    0.32, 1
-            bezier = easeInOutCubic, 0.65, 0.05, 0.36, 1
-            bezier = linear,         0,    0,    1,    1
-            bezier = almostLinear,   0.5,  0.5,  0.75, 1
-            bezier = quick,          0.15, 0,    0.1,  1
-
-            # Default animations, see https://wiki.hypr.land/Configuring/Animations/
-            #           NAME,          ONOFF, SPEED, CURVE,        [STYLE]
-            animation = global,        1,     10,    default
-            animation = border,        1,     5.39,  easeOutQuint
-            animation = windows,       1,     4.79,  easeOutQuint
-            animation = windowsIn,     1,     4.1,   easeOutQuint, popin 87%
-            animation = windowsOut,    1,     1.49,  linear,       popin 87%
-            animation = fadeIn,        1,     1.73,  almostLinear
-            animation = fadeOut,       1,     1.46,  almostLinear
-            animation = fade,          1,     3.03,  quick
-            animation = layers,        1,     3.81,  easeOutQuint
-            animation = layersIn,      1,     4,     easeOutQuint, fade
-            animation = layersOut,     1,     1.5,   linear,       fade
-            animation = fadeLayersIn,  1,     1.79,  almostLinear
-            animation = fadeLayersOut, 1,     1.39,  almostLinear
-            animation = workspaces,    1,     1.94,  almostLinear, fade
-            animation = workspacesIn,  1,     1.21,  almostLinear, fade
-            animation = workspacesOut, 1,     1.94,  almostLinear, fade
-            animation = zoomFactor,    1,     7,     quick
-        }
 
         # Ref https://wiki.hypr.land/Configuring/Workspace-Rules/
         # "Smart gaps" / "No gaps when only"
@@ -392,7 +393,6 @@ in
 
         # Fix some dragging issues with XWayland
         windowrule = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
-
       '';
     };
   };
