f4af5356e803b16399cd3aae2f0675e65babf3c7
John Murray <5672686+JRMurr@users.noreply.github.com>
2026-01-02 23:38:00 -0500

    more customizing

--- common/homemanager/hyprland/hyprpanel/default.nix
+++ common/homemanager/hyprland/hyprpanel/default.nix
@@ -54,6 +54,32 @@ let
       inherit name value;
     };
 
+  panelPkgs = pkgs.callPackage ./patched-panel.nix { };
+
+  catCfg = config.catppuccin;
+
+  # builtins.trace "maybe????: ${catCfg.flavor}-${catCfg.accent}" ---- mocha-mauve
+
+  themeFile = "catppuccin_${catCfg.flavor}.json";
+
+  themeJson = builtins.fromJSON (builtins.readFile "${panelPkgs}/share/themes/${themeFile}");
+
+  # toShortcutAttrs :: [a] -> { shortcut1 = a; … }
+  toShortcutAttrs =
+    list:
+    let
+      len = builtins.length list;
+    in
+    if len > 4 then
+      builtins.throw "toShortcutAttrs: expected at most 4 values, got ${toString len}"
+    else
+      builtins.listToAttrs (
+        lib.imap1 (idx: val: {
+          name = "shortcut${toString idx}";
+          value = val;
+        }) list
+      );
+
 in
 {
 
@@ -64,49 +90,88 @@ in
   };
 
   programs.hyprpanel = {
-    package = pkgs.callPackage ./patched-panel.nix { };
+    package = panelPkgs;
     enable = true;
     # Configure and theme almost all options from the GUI.
     # See 'https://hyprpanel.com/configuration/settings.html'.
 
     # can find the keys by looking at the settings dialog src
     # https://github.com/Jas-SinghFSU/HyprPanel/tree/master/src/components/settings/pages/config
-
-    settings = {
-      scalingPriority = "hyprland";
-      bar = {
-        layouts = lib.listToAttrs (map monitorToBarCfg monitors);
-        launcher.autoDetectIcon = true;
-        workspaces = {
-          monitorSpecific = true;
-          show_numbered = true;
-          show_icons = false;
-          ignored = "-.*"; # hack to hide the special kitty workspace. It seems to get a negative number?
-        };
-      };
-
-      menus.clock = {
-        time = {
-          military = false;
-          hideSeconds = true;
+    settings = lib.mkMerge [
+      themeJson
+      {
+        scalingPriority = "hyprland";
+        bar = {
+          layouts = lib.listToAttrs (map monitorToBarCfg monitors);
+          launcher = {
+            autoDetectIcon = true;
+          };
+          workspaces = {
+            monitorSpecific = true;
+            show_numbered = true;
+            show_icons = false;
+            ignored = "-.*"; # hack to hide the special kitty workspace. It seems to get a negative number?
+          };
         };
 
-        weather = {
-          enabled = true;
-          location = "Washington DC";
-          key = config.age.secrets.weather.path;
+        menus = {
+          clock = {
+            time = {
+              military = false;
+              hideSeconds = true;
+            };
+
+            weather = {
+              enabled = true;
+              location = "Washington DC";
+              key = config.age.secrets.weather.path;
+            };
+          };
+
+          dashboard = {
+            directories = {
+              enabled = false;
+            };
+            stats = {
+              enable_gpu = false; # TODO: still doesnt work with cuda support in the pkg
+            };
+
+            shortcuts = {
+              enabled = true;
+              # https://www.nerdfonts.com/cheat-sheet
+              left = toShortcutAttrs [
+                {
+                  icon = "";
+                  command = "firefox";
+                  tooltip = "Firefox";
+                }
+                {
+                  icon = "";
+                  command = "pear-desktop";
+                  tooltip = "Youtube Music";
+                }
+                {
+                  icon = "";
+                  command = "discord";
+                  tooltip = "Discord";
+                }
+                {
+                  icon = "";
+                  command = "rofi -show run";
+                  tooltip = "Search Apps";
+                }
+              ];
+            };
+          };
         };
-      };
 
-      menus.dashboard.directories.enabled = false;
-      menus.dashboard.stats.enable_gpu = false; # TODO: still doesnt work with cuda support in the pkg
+        # theme.bar.transparent = true;
 
-      # theme.bar.transparent = true;
-
-      # theme.font = {
-      #   name = "CaskaydiaCove NF";
-      #   size = "16px";
-      # };
-    };
+        # theme.font = {
+        #   name = "CaskaydiaCove NF";
+        #   size = "16px";
+        # };
+      }
+    ];
   };
 }
