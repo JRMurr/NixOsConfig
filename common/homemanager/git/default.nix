1ced39d55101df8b9ebb6e0f613c08644dea4efc
John Murray <5672686+JRMurr@users.noreply.github.com>
2026-02-06 15:46:30 -0500

    fix escaping

--- common/homemanager/git/default.nix
+++ common/homemanager/git/default.nix
@@ -32,7 +32,7 @@ in
         yoink = "pull";
         yeet = "push";
         gh = "!open $(fish -c git_repo_url)";
-        lb = "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\\\033[33m%s: \\\\033[37m %s\\\\033[0m\\\\n\", substr($2, 1, length($2)-1), $1)}'";
+        lb = "!git reflog show --pretty=format:'%gs ~ %gd' --date=relative | grep 'checkout:' | grep -oE '[^ ]+ ~ .*' | awk -F~ '!seen[$1]++' | head -n 10 | awk -F' ~ HEAD@{' '{printf(\"  \\033[33m%s: \\033[37m %s\\033[0m\\n\", substr($2, 1, length($2)-1), $1)}'";
         tmp = "!git add -A && git commit --no-verify -m 'tmp'";
         scary = "!git fetch && git reset --hard origin/$(git rev-parse --abbrev-ref HEAD)";
         forget = "update-index --skip-worktree";
